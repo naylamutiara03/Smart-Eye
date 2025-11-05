be9e4a4d1dba9821b6283e60907b5e5a3fde897a
Nayla Mutiara Salsabila Bastari <naylamsb03@gmail.com>
2025-11-05 15:11:41 +0700

    Revert "add fungsi login dan regist"
    This reverts commit aa0a22771033cdc072afe951b7556857c18752f5.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,22 @@
 // frontend/src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Detect from './pages/Detect';
 import History from './pages/History';
-import Login from './pages/Login';
-import Register from './pages/Register';
 
-function AppContent() {
-  const location = useLocation();
-
-  // daftar halaman yang tidak menampilkan navbar
-  const hideNavbarRoutes = ['/login', '/register'];
-  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
-
+function App() {
   return (
-    <>
-      {!shouldHideNavbar && <Navbar />}
-
-      <main className={`${shouldHideNavbar ? '' : 'container mt-4'}`}>
+    <Router>
+      <Navbar />
+      <main className="container mt-4">
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
           <Route path="/detect" element={<Detect />} />
           <Route path="/history" element={<History />} />
         </Routes>
       </main>
-    </>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <AppContent />
     </Router>
   );
 }
